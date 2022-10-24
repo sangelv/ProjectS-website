014ca1b9c5c98dde4b24f4268cdffff7d364e6fa
sangelv <92814055+sangelv@users.noreply.github.com>
2022-10-24 14:36:59 +0200

    24/10/22.2

--- index.js
+++ index.js
@@ -10,16 +10,14 @@ function setSceneMap() {
   document.getElementById("sceneMap").setAttribute("visible", "true");
   document.getElementById("scene2").setAttribute("visible", "false");
   document.getElementById("scene2hdgd").setAttribute("visible", "false");
+  document.getElementById("scene5").setAttribute("visible", "false");
+  document.getElementById("scene5hdgd").setAttribute("visible", "false");
   document.getElementById("scene3").setAttribute("visible", "false");
   document.getElementById("scene3hdgd").setAttribute("visible", "false");
   document.getElementById("scene4").setAttribute("visible", "false");
   document.getElementById("scene4hdgd").setAttribute("visible", "false");
-  document.getElementById("scene5").setAttribute("visible", "false");
-  document.getElementById("scene5hdgd").setAttribute("visible", "false");
   document.getElementById("human1").setAttribute("src", "");
   
-  
- 
 }
 
 function setScene2() {
@@ -27,6 +25,7 @@ function setScene2() {
   document.getElementById("scene2").setAttribute("visible", "true");
   document.getElementById("scene2hdgd").setAttribute("visible", "false");
   document.getElementById("scene5").setAttribute("visible", "false");
+  document.getElementById("scene5hdgd").setAttribute("visible", "false");
   document.getElementById("human1").setAttribute("src", "");
   
   
@@ -37,9 +36,49 @@ function setScene2HG() {
   document.getElementById("scene2").setAttribute("visible", "false");
   document.getElementById("scene2hdgd").setAttribute("visible", "true");
   document.getElementById("scene5").setAttribute("visible", "false");
-  document.getElementById("human1").setAttribute("src", "");
+  
 }
 
+function setScene5() {
+  document.getElementById("sceneMap").setAttribute("visible", "false");
+  document.getElementById("scene5").setAttribute("visible", "true");
+  document.getElementById("scene5hdgd").setAttribute("visible", "false");
+  document.getElementById("scene2").setAttribute("visible", "false");
+  document.getElementById("scene2hdgd").setAttribute("visible", "false");
+  document.getElementById("scene4").setAttribute("visible", "false");
+  document.getElementById("human2").setAttribute("src", "");
+ 
+  
+}
+
+function setScene5HG() {
+  document.getElementById("sceneMap").setAttribute("visible", "false");
+  document.getElementById("scene5").setAttribute("visible", "false");
+  document.getElementById("scene5hdgd").setAttribute("visible", "true");
+  document.getElementById("scene4").setAttribute("visible", "false");
+  
+}
+
+
+function setScene4() {
+  document.getElementById("sceneMap").setAttribute("visible", "false");
+  document.getElementById("scene4").setAttribute("visible", "true");
+  document.getElementById("scene4hdgd").setAttribute("visible", "false");
+  document.getElementById("scene3").setAttribute("visible", "false");
+  document.getElementById("scene5").setAttribute("visible", "false");
+  document.getElementById("scene5hdgd").setAttribute("visible", "false");
+  document.getElementById("human3").setAttribute("src", "");
+  
+}
+
+function setScene4HG() {
+  document.getElementById("sceneMap").setAttribute("visible", "false");
+  document.getElementById("scene4").setAttribute("visible", "false");
+  document.getElementById("scene4hdgd").setAttribute("visible", "true");
+  document.getElementById("scene3").setAttribute("visible", "false");
+  document.getElementById("human1").setAttribute("src", "");
+
+}
 
 
 function setScene3() {
@@ -48,7 +87,7 @@ function setScene3() {
   document.getElementById("scene3hdgd").setAttribute("visible", "false");
   document.getElementById("scene4").setAttribute("visible", "false");
   document.getElementById("scene4hdgd").setAttribute("visible", "false");
-  document.getElementById("human1").setAttribute("src", "");
+  document.getElementById("human4").setAttribute("src", "");
   
 }
 
@@ -62,42 +101,6 @@ function setScene3HG() {
   
 }
 
-function setScene4() {
-  document.getElementById("sceneMap").setAttribute("visible", "false");
-  document.getElementById("scene4").setAttribute("visible", "true");
-  document.getElementById("scene4hdgd").setAttribute("visible", "false");
-  document.getElementById("scene3").setAttribute("visible", "false");
-  document.getElementById("scene5").setAttribute("visible", "false");
-  document.getElementById("scene5hdgd").setAttribute("visible", "false");
-  document.getElementById("human1").setAttribute("src", "");
-  
-}
 
-function setScene4HG() {
-  document.getElementById("sceneMap").setAttribute("visible", "false");
-  document.getElementById("scene4").setAttribute("visible", "false");
-  document.getElementById("scene4hdgd").setAttribute("visible", "true");
-  document.getElementById("scene3").setAttribute("visible", "false");
-  document.getElementById("human1").setAttribute("src", "");
-  
-}
 
-function setScene5() {
-  document.getElementById("sceneMap").setAttribute("visible", "false");
-  document.getElementById("scene5").setAttribute("visible", "true");
-  document.getElementById("scene5hdgd").setAttribute("visible", "false");
-  document.getElementById("scene2").setAttribute("visible", "false");
-  document.getElementById("scene2hdgd").setAttribute("visible", "false");
-  document.getElementById("scene4").setAttribute("visible", "false");
-  document.getElementById("human1").setAttribute("src", "");
-  
-}
 
-function setScene5HG() {
-  document.getElementById("sceneMap").setAttribute("visible", "false");
-  document.getElementById("scene5").setAttribute("visible", "false");
-  document.getElementById("scene5hdgd").setAttribute("visible", "true");
-  document.getElementById("scene4").setAttribute("visible", "false");
-  document.getElementById("human1").setAttribute("src", "");
-  
-}
