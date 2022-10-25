d0a6aa93f7b1c615d6a41b08a6bd8d5d8b21cd74
sangelv <92814055+sangelv@users.noreply.github.com>
2022-10-25 13:26:51 +0200

    20/10/22.1

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ function setSceneStart() {
   
 }
 
+window.addEventListener('touchstart', function() {
+
+	// create empty buffer
+	var buffer = myContext.createBuffer(1, 1, 22050);
+	var source = myContext.createBufferSource();
+	source.buffer = buffer;
+
+	// connect to output (your speakers)
+	source.connect(myContext.destination);
+
+	// play the file
+	source.noteOn(0);
+
+}, false);
+
 
 function setSceneMap() {
   document.getElementById("sceneStart").setAttribute("visible", "false");
