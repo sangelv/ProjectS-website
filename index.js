992e76227a4b0316b89a75d77f8dbf7b313cb145
sangelv <92814055+sangelv@users.noreply.github.com>
2022-10-25 13:33:44 +0200

    24/20/22.2

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+
+var isUnlocked = false;
+function unlock() {
+			
+	if(isIOS || this.unlocked)
+		return;
+
+	// create empty buffer and play it
+	var buffer = myContext.createBuffer(1, 1, 22050);
+	var source = myContext.createBufferSource();
+	source.buffer = buffer;
+	source.connect(myContext.destination);
+	source.noteOn(0);
+
+	// by checking the play state after some time, we know if we're really unlocked
+	setTimeout(function() {
+		if((source.playbackState === source.PLAYING_STATE || source.playbackState === source.FINISHED_STATE)) {
+			isUnlocked = true;
+		}
+	}, 0);
+
+}
+
 function setSceneStart() {
   document.getElementById("sceneStart").setAttribute("visible", "true");
   document.getElementById("sceneMap").setAttribute("visible", "false");
@@ -5,20 +28,6 @@ function setSceneStart() {
   
 }
 
-window.addEventListener('touchstart', function() {
-
-	// create empty buffer
-	var buffer = myContext.createBuffer(1, 1, 22050);
-	var source = myContext.createBufferSource();
-	source.buffer = buffer;
-
-	// connect to output (your speakers)
-	source.connect(myContext.destination);
-
-	// play the file
-	source.noteOn(0);
-
-}, false);
 
 
 function setSceneMap() {
